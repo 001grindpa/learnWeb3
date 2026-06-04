c150c4298c1a4abfd674a49b133fc48b5faf3162
Grindpa <anyanwufrancischu@yahoo.com>
2026-06-04 20:16:51 +0100

    update: implemented a sign system that allows another account to claim for an eligible user

--- solidity/Mercle-airdrop/src/MerkleAirdrop.sol
+++ solidity/Mercle-airdrop/src/MerkleAirdrop.sol
@@ -2,16 +2,28 @@
 pragma solidity ^0.8.18;
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract MerkleAirdrop {
+contract MerkleAirdrop is EIP712 {
     // == ERRORS == //
     error MerkleAirdrop__InvaidProof(address account);
     error MerkleAirdrop__UserAlreadyClaimed();
+    error MerkleAirdrop__InvalidSignature();
 
+    // == CUSTOM DATATYPES == //
+    /**@dev This is a struct of the txn message data that'll be hashed and used */
+    struct AirdropClaim {
+        address account;
+        uint256 amount;
+    }
+
+    // == STATE VARIABLES == //
     // address[] claimers;
     bytes32 private immutable I_MERKLE_ROOT;
     IERC20 private immutable I_CHIMCHIM_TOKEN;
     mapping(address claimer => bool claimed) public sClaimedUsers;
+    bytes32 private constant MESSAGE_TYPEHASH = keccak256("AirdropClaim(address account, uint256 amount)");
 
     // == SYNTACTIC SUGAR == //
     using SafeERC20 for IERC20;
@@ -19,19 +31,31 @@ contract MerkleAirdrop {
     // == EVENTS == //
     event ProofConfirmed(address indexed user, uint256 indexed amount);
 
-    constructor(bytes32 merkleRoot, address airdropToken) {
+    constructor(bytes32 merkleRoot, address airdropToken) EIP712("Merkle Airdrop", "1") {
         I_MERKLE_ROOT = merkleRoot;
         I_CHIMCHIM_TOKEN = IERC20(airdropToken);
     }
 
-    function claim(address account, uint256 amount, bytes32[] calldata merkleProofs) external returns (bool) {
+    /** @dev This function uses EIP-712 standard to approve that a different account can claim for an
+    eligible user and pay for their claiming gas, if the eligible user signs an approval to them.
+     @param v This is the first signature
+     @param r This is the second signature
+     @param s This is the third signature
+     @notice These signatures were generated during the signing process and will be used to confirm the signer
+     (using an openzeppelin method that utilises ecrecover() function), then grant the secondry account 
+     access to claim the tokens for the eigible user */
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProofs, uint8 v, bytes32 r, bytes32 s) external returns (bool) {
         if (sClaimedUsers[msg.sender] == true) {
             revert MerkleAirdrop__UserAlreadyClaimed();
         }
+        // revert if the signature is invalid
+        if (!_isValidSignature(account, getMessageHash(account, amount), v, r, s)) {
+            revert MerkleAirdrop__InvalidSignature();
+        }
         // at this point we've already prepared the merkle tree offchain and gotten the original root.
         // Provided with the user address and their eligible amount, alongside the array of
-        // sibling leaf hashes we'll use for climbing up the tree untill we get to the equivalent root branch hash
-        // we'll hash the address and amount then use that to climb the tree by hashing the computed hash with
+        // sibling leaf hashes we'll use for climbing up the tree untill we get to the equivalent root branch hash.
+        // we'll hash the address and amount then use that to climb the tree by hashing the computed hash(digest) with
         // the first array sibling leaf hash, and continue that for the next sibling and so forth.
         // we're using a double-hash optimisation for extra uniqueness
         bytes32 leaf = keccak256(abi.encodePacked(keccak256(abi.encode(account, amount))));
@@ -48,6 +72,25 @@ contract MerkleAirdrop {
         // we're using safe transfer so it handles errors better
         I_CHIMCHIM_TOKEN.safeTransfer(account, amount);
         return true;
+    }
+
+
+    function getMessageHash(address account, uint256 amount) public view returns (bytes32) {
+        return _hashTypedDataV4(
+            keccak256(
+                abi.encode(MESSAGE_TYPEHASH, AirdropClaim({
+                    account: account,
+                    amount: amount
+                }))
+            )
+        );
+    }
+
+    // == PRIVATE/INTERNAL FUNCTIONS == //
+    /**@notice This function is used to verify a ERC712 signature */
+    function _isValidSignature(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s) internal view returns (bool) {
+        (address actualSigner,,) = ECDSA.tryRecover(digest, v, r, s);
+        return actualSigner == account;
     }
 
     // == GETTERS == //
--- solidity/Mercle-airdrop/test/unit/TestMerkleAirdrop.t.sol
+++ solidity/Mercle-airdrop/test/unit/TestMerkleAirdrop.t.sol
@@ -10,12 +10,23 @@ contract TestMerkleAirdrop is Test {
     MerkleAirdrop public merkleAirdrop;
     ChimChimToken public cct;
     DeployMerkleAirdrop public deployer;
+    address public user;
+    uint256 public pk;
+    address public GAS_PAYER;
 
     bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     bytes32[] public PROOFS = [bytes32(0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a),
     bytes32(0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576)];
 
     function setUp() public {
+        // our addy and pk
+        (user, pk) = makeAddrAndKey("user");
+        // this user will claim for us
+        GAS_PAYER = makeAddr("gasPayer");
+        // fund gaPayer and user
+        vm.deal(GAS_PAYER, 1 ether);
+        vm.deal(user, 1 ether);
+
         cct = new ChimChimToken();
         deployer = new DeployMerkleAirdrop();
         (merkleAirdrop, cct) = deployer.run();
@@ -31,14 +42,20 @@ contract TestMerkleAirdrop is Test {
         assertEq(receivedRoot, ROOT);
     }
 
-    function testUserIsEligible() public {
+    function testUserIsEligibleAndClaim() public {
         uint256 contractStartingBal = cct.balanceOf(address(merkleAirdrop));
-        address user = 0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D;
         uint256 eligibleAmount = 25000000000000000000; // 25e18
         uint256 initialBal = cct.balanceOf(user);
+        
 
-        vm.prank(user);
-        bool status = merkleAirdrop.claim(user, eligibleAmount, PROOFS);
+        vm.startPrank(user);
+        // get messageData hash
+        bytes32 digest = merkleAirdrop.getMessageHash(user, eligibleAmount);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(pk, digest);
+        vm.stopPrank();
+
+        vm.prank(GAS_PAYER);
+        bool status = merkleAirdrop.claim(user, eligibleAmount, PROOFS, v, r, s);
         uint256 currentBal = cct.balanceOf(user);
         uint256 contractEndingBal = cct.balanceOf(address(merkleAirdrop));
         
